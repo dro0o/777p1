0d12c92d21e48bc205e74be017d5773907467618
Andrew Pittman <andrew@pittman.dev>
2020-02-17 11:17:50 -0500

    import shp zips, convert to geojson

--- src/MapboxGLMap.js
+++ src/MapboxGLMap.js
@@ -1,12 +1,39 @@
 import React, { useEffect, useRef, useState } from "react"
 import mapboxgl from "mapbox-gl"
 import "mapbox-gl/dist/mapbox-gl.css"
+import shp from "shpjs"
 
 const styles = {
   width: "100vw",
   height: "calc(100vh - 64px)",
   position: "absolute"
 }
+
+// Import shapefile zips
+shp(process.env.PUBLIC_URL + "/shp/well_nitrate.zip").then(
+  function(geojsonWellNitrate) {
+    console.log("well nitrate: ", geojsonWellNitrate)
+  },
+  function(err) {
+    console.log(err)
+  }
+)
+shp(process.env.PUBLIC_URL + "/shp/cancer_county.zip").then(
+  function(geojsonCancerCounty) {
+    console.log("cancer county: ", geojsonCancerCounty)
+  },
+  function(err) {
+    console.log(err)
+  }
+)
+shp(process.env.PUBLIC_URL + "/shp/cancer_tracts.zip").then(
+  function(geojsonCancerTracts) {
+    console.log("cancer tracts: ", geojsonCancerTracts)
+  },
+  function(err) {
+    console.log(err)
+  }
+)
 
 const MapboxGLMap = () => {
   const [map, setMap] = useState(null)
@@ -18,11 +45,18 @@ const MapboxGLMap = () => {
   useEffect(() => {
     mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
     const initializeMap = ({ setMap, mapContainer }) => {
+      var bounds = [
+        [-95, 40], // Southwest coordinates
+        [-85, 48] // Northeast coordinates
+      ]
+
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: "mapbox://styles/mapbox/dark-v10", // stylesheet location
         center: [lng, lat],
-        zoom: zoom
+        zoom: zoom,
+        minZoom: 6.5,
+        maxBounds: bounds
       })
 
       map.on("load", () => {
