1a1ca120e9e7574c93b3c9dc56ee16719155c9f0
Andrew Pittman <andrew@pittman.dev>
2020-02-16 19:44:13 -0500

    add content to about splash

--- src/ProjectAppBar.js
+++ src/ProjectAppBar.js
@@ -11,6 +11,15 @@ import Typography from "@material-ui/core/Typography"
 import Badge from "@material-ui/core/Badge"
 import HelpOutlineIcon from "@material-ui/icons/HelpOutline"
 import Backdrop from "@material-ui/core/Backdrop"
+import Card from "@material-ui/core/Card"
+import CardActionArea from "@material-ui/core/CardActionArea"
+import CardActions from "@material-ui/core/CardActions"
+import CardContent from "@material-ui/core/CardContent"
+import CardMedia from "@material-ui/core/CardMedia"
+import Button from "@material-ui/core/Button"
+import GitHubIcon from "@material-ui/icons/GitHub"
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import EmailIcon from "@material-ui/icons/Email"
 
 const useStyles = makeStyles(uwTheme => ({
   grow: {
@@ -21,23 +30,23 @@ const useStyles = makeStyles(uwTheme => ({
     paddingRight: 15
   },
   title: {
-    display: "none",
-    [uwTheme.breakpoints.up("sm")]: {
-      display: "block"
-    }
+    display: "block"
   },
   inputRoot: {
     color: "inherit"
   },
   sectionDesktop: {
-    display: "none",
-    [uwTheme.breakpoints.up("md")]: {
-      display: "flex"
-    }
+    display: "flex"
   },
   backdrop: {
     zIndex: uwTheme.zIndex.drawer + 1,
     color: "#fff"
+  },
+  card: {
+    maxWidth: 420
+  },
+  media: {
+    height: 140
   }
 }))
 
@@ -76,7 +85,7 @@ export default function PrimarySearchAppBar() {
               className={classes.logo}
             />
             <Typography className={classes.title} variant="h6" noWrap>
-              Wisconsin Spatial Analysis: Nitrate & Cancer
+              Spatial Analysis: Nitrate & Cancer
             </Typography>
             <div className={classes.grow} />
             <div className={classes.sectionDesktop}>
@@ -96,7 +105,95 @@ export default function PrimarySearchAppBar() {
           className={classes.backdrop}
           open={bdOpen}
           onClick={handleBDClose}
-        ></Backdrop>
+        >
+          <Card className={classes.card}>
+            <CardActionArea>
+              <CardMedia
+                className={classes.media}
+                image="nitrate_personal.png"
+                title="Contemplative Reptile"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  Spatial Analysis: Nitrate & Cancer
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  gutterBottom
+                >
+                  This web application was created to aid in exploration and
+                  visualization of a potential relationship between water
+                  nitrate levels and cancer rates in Wisconsin. The data used in
+                  the analysis: 1) locations of cancer occurrences collected
+                  over a ten-year period 2) nitrate levels from test wells
+                  throughout each county in the state of Wisconsin.
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  gutterBottom
+                >
+                  The application uses Inverse Distance Weighted (IDW)
+                  interpolation technique to produce a raster map of nitrate
+                  levels in WI. This technique is explicit in implying that
+                  close things are more related than distant things and
+                  allocates weights quantitatively based on distance. The second
+                  analysis layer in the application produces an Ordinary Least
+                  Squares linear regression to model a dependent variable
+                  (cancer rates) in terms of its relationship to the explanatory
+                  variable (nitrate levels).
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  gutterBottom
+                >
+                  Authored by Andrew Pittman as a part of capstone project for
+                  the University of Wisconsin Madison.
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+            <CardActions>
+              <IconButton
+                variant="link"
+                aria-label="GitHubIcon"
+                color="inherit"
+                href="https://github.com/dro0o"
+                target="_blank"
+              >
+                <Badge color="secondary">
+                  <GitHubIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                variant="link"
+                aria-label="LinkedInIcon"
+                color="inherit"
+                href="https://www.linkedin.com/in/andrew-pittman-a1618922/"
+                target="_blank"
+              >
+                <Badge color="secondary">
+                  <LinkedInIcon />
+                </Badge>
+              </IconButton>
+              <IconButton
+                variant="link"
+                aria-label="EmailIcon"
+                color="inherit"
+                href="mailto: andrew@pittman.dev"
+                target="_blank"
+              >
+                <Badge color="secondary">
+                  <EmailIcon />
+                </Badge>
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Backdrop>
       </ThemeProvider>
     </div>
   )
