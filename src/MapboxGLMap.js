d01a2cd2d2cfed004ffbed4556c3d8eb71001ff6
Andrew Pittman <andrew@pittman.dev>
2020-02-21 22:02:14 -0500

    mod color scale values

--- src/MapboxGLMap.js
+++ src/MapboxGLMap.js
@@ -101,8 +101,8 @@ const uwTheme2 = createMuiTheme({
 
 const options = [
   {
-    name: "Population",
-    description: "Estimated total population",
+    name: "Spatial Regression",
+    description: "R^2 Value for each polygon",
     property: "pop_est",
     stops: [
       [0, "#f8d5cc"],
@@ -114,22 +114,6 @@ const options = [
       [250000000, "#c44cc0"],
       [500000000, "#9f43d7"],
       [1000000000, "#6e40e6"]
-    ]
-  },
-  {
-    name: "GDP",
-    description: "Estimate total GDP in millions of dollars",
-    property: "gdp_md_est",
-    stops: [
-      [0, "#f8d5cc"],
-      [1000, "#f4bfb6"],
-      [5000, "#f1a8a5"],
-      [10000, "#ee8f9a"],
-      [50000, "#ec739b"],
-      [100000, "#dd5ca8"],
-      [250000, "#c44cc0"],
-      [5000000, "#9f43d7"],
-      [10000000, "#6e40e6"]
     ]
   }
 ]
