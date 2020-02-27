68cec509ad831f347c0043aa22efd5a723cf7b71
Andrew Pittman <andrew@pittman.dev>
2020-02-26 21:52:50 -0500

    add portfolio link

--- src/ProjectAppBar.js
+++ src/ProjectAppBar.js
@@ -19,6 +19,7 @@ import CardMedia from "@material-ui/core/CardMedia"
 import GitHubIcon from "@material-ui/icons/GitHub"
 import LinkedInIcon from "@material-ui/icons/LinkedIn"
 import EmailIcon from "@material-ui/icons/Email"
+import AccountCircleIcon from "@material-ui/icons/AccountCircle"
 
 const useStyles = makeStyles(uwTheme => ({
   grow: {
@@ -122,12 +123,13 @@ export default function PrimarySearchAppBar() {
                   component="p"
                   gutterBottom
                 >
-                  This web application was created to aid in exploration and
+                  This web application was developed to enable exploration and
                   visualization of a potential relationship between water
-                  nitrate levels and cancer rates in Wisconsin. The data used in
-                  the analysis: 1) locations of cancer occurrences collected
-                  over a ten-year period 2) nitrate levels from test wells
-                  throughout each county in the state of Wisconsin.
+                  nitrate levels and cancer rates in Wisconsin. The analysis
+                  data includes normalized cancer occurrence rates aggregated by
+                  census tract and collected over a ten-year period, as well as
+                  nitrate levels from test wells throughout each county in the
+                  state.
                 </Typography>
                 <Typography
                   variant="body2"
@@ -135,15 +137,16 @@ export default function PrimarySearchAppBar() {
                   component="p"
                   gutterBottom
                 >
-                  The application uses Inverse Distance Weighted (IDW)
-                  interpolation technique to produce a raster map of nitrate
-                  levels in WI. This technique is explicit in implying that
-                  close things are more related than distant things and
-                  allocates weights quantitatively based on distance. The second
-                  analysis layer in the application produces an Ordinary Least
-                  Squares linear regression to model a dependent variable
-                  (cancer rates) in terms of its relationship to the explanatory
-                  variable (nitrate levels).
+                  The application uses the Inverse Distance Weighted (IDW)
+                  interpolation technique to produce two spatial aggregations,
+                  one of nitrate levels and one of normalized cancer rates. The
+                  IDW method is explicit in implying that close things are more
+                  related than distant things and allocates weights
+                  quantitatively based on distance. The resulting analysis layer
+                  leverages the two spatial aggregations to produced an Ordinary
+                  Least Squares (OLS) linear regression to model a dependent
+                  variable (normalized cancer rates) in terms of its
+                  relationship to the explanatory variable (nitrate levels).
                 </Typography>
                 <Typography
                   variant="body2"
@@ -151,12 +154,23 @@ export default function PrimarySearchAppBar() {
                   component="p"
                   gutterBottom
                 >
-                  Authored by Andrew Pittman as a part of capstone project for
+                  Developed by Andrew Pittman as a part of capstone project for
                   the University of Wisconsin Madison.
                 </Typography>
               </CardContent>
             </CardActionArea>
             <CardActions>
+              <IconButton
+                variant="link"
+                aria-label="PortfolioIcon"
+                color="inherit"
+                href="https://adp6729.github.io/react-mdl-portfolio/"
+                target="_blank"
+              >
+                <Badge color="secondary">
+                  <AccountCircleIcon />
+                </Badge>
+              </IconButton>
               <IconButton
                 variant="link"
                 aria-label="GitHubIcon"
